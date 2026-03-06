97e1b93c6a651e94029518cdba8581d8d2d00de5
abhibs <abhirambs97@gmail.com>
2026-03-06 16:02:36 +0530

    admin dashboard segmentation

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,15 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('admin.layout.app')
+@section('content')
+    <div class="page-content">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
+            <div class="col">
+                <div class="card radius-10 border-start border-0 border-4 border-info">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div>
+                                <p class="mb-0 text-secondary">Total Orders</p>
+                                <h4 class="my-1 text-info">4805</h4>
+                                <p class="mb-0 font-13">+2.5% from last week</p>
+                            </div>
+                            <div class="widgets-icons-2 rounded-circle bg-gradient-blues text-white ms-auto"><i
+                                    class='bx bxs-cart'></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card radius-10 border-start border-0 border-4 border-danger">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div>
+                                <p class="mb-0 text-secondary">Total Revenue</p>
+                                <h4 class="my-1 text-danger">$84,245</h4>
+                                <p class="mb-0 font-13">+5.4% from last week</p>
+                            </div>
+                            <div class="widgets-icons-2 rounded-circle bg-gradient-burning text-white ms-auto">
+                                <i class='bx bxs-wallet'></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card radius-10 border-start border-0 border-4 border-success">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div>
+                                <p class="mb-0 text-secondary">Bounce Rate</p>
+                                <h4 class="my-1 text-success">34.6%</h4>
+                                <p class="mb-0 font-13">-4.5% from last week</p>
+                            </div>
+                            <div class="widgets-icons-2 rounded-circle bg-gradient-ohhappiness text-white ms-auto">
+                                <i class='bx bxs-bar-chart-alt-2'></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card radius-10 border-start border-0 border-4 border-warning">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div>
+                                <p class="mb-0 text-secondary">Total Customers</p>
+                                <h4 class="my-1 text-warning">8.4K</h4>
+                                <p class="mb-0 font-13">+8.4% from last week</p>
+                            </div>
+                            <div class="widgets-icons-2 rounded-circle bg-gradient-orange text-white ms-auto">
+                                <i class='bx bxs-group'></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--end row-->
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
 
-<body>
-    <h1>Admin Dashboard</h1>
-</body>
 
-</html>
+    </div>
+@endsection
---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,28 @@
+        <div class="sidebar-wrapper" data-simplebar="true">
+            <div class="sidebar-header">
+                <div>
+                    <img src="{{ asset('admin/assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
+                </div>
+                <div>
+                    <h4 class="logo-text">Rocker</h4>
+                </div>
+                <div class="toggle-icon ms-auto"><i class='bx bx-arrow-back'></i>
+                </div>
+            </div>
+            <!--navigation-->
+            <ul class="metismenu" id="menu">
+                <li>
+                    <a href="{{ route('admin-dashboard') }}">
+                        <div class="parent-icon"><i class='bx bx-home-alt'></i>
+                        </div>
+                        <div class="menu-title">Dashboard</div>
+                    </a>
+
+                </li>
+
+
+
+
+            </ul>
+            <!--end navigation-->
+        </div>
