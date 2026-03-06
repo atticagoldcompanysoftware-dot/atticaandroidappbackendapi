357dfddac5af70c52314e40ca1ad5e27ace90ff4
abhibs <abhirambs97@gmail.com>
2026-03-06 15:36:26 +0530

    admin login page

--- routes/admin.php
+++ routes/admin.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Admin\AdminController;
 
 Route::get('/test', function () {
     return "Abhiram";
 });
+
+Route::group(
+    ['prefix' => 'admin'],
+    function () {
+        Route::get('/login', [AdminController::class, 'login'])->name('admin-login');
+    }
+);
