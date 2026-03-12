4baf9afb814ef80573af89b35733f8655f374e13
abhibs <abhirambs97@gmail.com>
2026-03-12 17:32:17 +0530

    product store

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -1,5 +1,7 @@
 @extends('admin.layout.app')
 @section('content')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
     <div class="page-content">
         <!--breadcrumb-->
         <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
@@ -142,10 +144,20 @@
                                         </div>
                                     </div>
 
+                                    <div class="row mb-3">
+                                        <div class="col-sm-3">
+                                            <h6 class="mb-0"> </h6>
+                                        </div>
+                                        <div class="col-sm-9 text-secondary">
+                                            <img id="showImage" src="{{ url('no_image.jpg') }} " alt="Admin"
+                                                style="width:100px; height: 100px;">
+                                        </div>
+                                    </div>
+
                                     <div class="row">
                                         <div class="col-sm-3"></div>
                                         <div class="col-sm-9 text-secondary">
-                                            <input type="submit" class="btn btn-primary px-4" value="Add Gold Rate" />
+                                            <input type="submit" class="btn btn-primary px-4" value="Add Product" />
                                         </div>
                                     </div>
                             </div>
@@ -164,4 +176,15 @@
             </div>
         </div>
     </div>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#image').change(function(e) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#showImage').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
+            });
+        });
+    </script>
 @endsection
